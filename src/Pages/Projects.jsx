8cc4e47e07c4051949ce883f4f30bbec42b00116
Augasta-sys <pswarnaaugasta@thestackly.com>
2026-07-06 15:09:29 +0530

    Add files via upload

---
+++ src/Pages/Projects.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../Components/Navbar";
+import ProjectHero from "../Components/ProjectHero";
+import ProjectGrid from "../Components/ProjectGrid";
+import Footer from "../Components/Footer";
+
+const Projects = () => {
+  return (
+    <>
+      <Navbar />
+      <ProjectHero />
+      <ProjectGrid />
+      <Footer />
+    </>
+  );
+};
+
+export default Projects;
